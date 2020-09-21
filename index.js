c99639db9ce158d2cc4c5ff0f4a054e569a2f561
Paul Panserrieu <paul.panserrieu@gmail.com>
2020-09-21 21:49:39 +0200

    delimiter is optional

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var ejs = require('ejs')
 
 module.exports = function(file, options) {
   
-  var match = file.match(/(.*\.ejs)@(\$|%)$/)
+  var match = file.match(/(.*\.ejs)(\$|%|)$/)
 
   if (!match) return through()
 
@@ -17,7 +17,7 @@ module.exports = function(file, options) {
     var template = ejs.compile(buffer, {
         client: true
       , compileDebug: options.debug || false
-      , delimiter: match[2] || '%'
+      , delimiter: match[2].length > 0 ? match[2] : '%'
       , filename: match[1]
     })
 
