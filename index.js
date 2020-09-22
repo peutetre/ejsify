5b4a3aefb2e000cca02a87406d1b08474e5f95dc
Paul Panserrieu <paul.panserrieu@gmail.com>
2020-09-22 11:17:41 +0200

    2.2.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var ejs = require('ejs')
   , through = require('through')
 
 module.exports = function(file, options) {
-  
+
   var match = file.match(/(.*\.ejs)(\$|%|)$/)
 
   if (!match) return through()
@@ -18,7 +18,8 @@ module.exports = function(file, options) {
         client: true
       , compileDebug: options.debug || false
       , delimiter: match[2].length > 0 ? match[2] : '%'
-      , filename: match[1]
+      , filename: file
+      , 'async': options.async || false
     })
 
     this.queue('module.exports = (' + template + ')')
